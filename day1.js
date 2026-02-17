4bdfe937fda4f26d16a91dffa6b0eba377c4950e
Muhammad Ahmed <ahmedmughal091104@gmail.com>
2026-02-17 21:07:05 +0500

    first commit

---
+++ day1.js
@@ -0,0 +1,26 @@
+// let age = null;
+// let name ;
+// let isTrue;
+// let arr = [1, 2, 3, 4, 5];
+// let obj = {
+//     name : 'ahmed',
+//     age : 30,
+//     isTrue : true
+// }
+// let obj2 = {
+//     name : 'mohamed',
+//     age : 25,
+//     isTrue : false
+// }
+// console.log(age , obj.name , obj.age , arr);
+
+// CASE IVESTIGATION  (convertions)
+
+let num = undefined;
+console.log(typeof num);
+console.log(typeof Number(num));
+console.log(typeof num); 
+console.log(Number(num));
+
+// CASE INVESTIGATION  
+
